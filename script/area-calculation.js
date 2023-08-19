6d977799b9da6681744e1c9c744e50b7a9d1e303
dipujafar <131136004+dipujafar@users.noreply.github.com>
2023-08-19 13:43:40 +0600

    completed

--- script/area-calculation.js
+++ script/area-calculation.js
@@ -1,62 +1,3 @@
-//Triangle Area
-function areaTriangleCalculation() {
-  const baseField = document.getElementById("triangle-base");
-  const baseValueText = baseField.value;
-  const baseValueNumber = parseFloat(baseValueText);
-
-  baseField.value = "";
-
-  if (isNaN(baseValueNumber)) {
-    alert("Please Enter a Number Value of Base");
-    return;
-  }
-
-  //height
-  const heightField = document.getElementById("triangle-height");
-  const heightValueText = heightField.value;
-  const heightValueNumber = parseFloat(heightValueText);
-
-  heightField.value = "";
-
-  if (isNaN(heightValueNumber)) {
-    alert("Please Enter a Number Value of Height");
-    return;
-  }
-
-  const area = 0.5 * baseValueNumber * heightValueNumber;
-  return area;
-}
-
-//Rectangle Area
-
-function areaRectangleCalculation() {
-  const widthField = document.getElementById("rectangle-width");
-  const widthValueText = widthField.value;
-  const widthValueNumber = parseFloat(widthValueText);
-
-  widthField.value = "";
-
-  if (isNaN(widthValueNumber)) {
-    alert("Please Enter a Number Value of Width");
-    return;
-  }
-
-  //height
-  const lengthField = document.getElementById("rectangle-length");
-  const lengthValueText = lengthField.value;
-  const lengthValueNumber = parseFloat(lengthValueText);
-
-  lengthField.value = "";
-
-  if (isNaN(lengthValueNumber)) {
-    alert("Please Enter a Number Value of Length");
-    return;
-  }
-
-  const area = widthValueNumber * lengthValueNumber;
-  return area;
-}
-
 function inputValue(fieldId) {
   const inputField = document.getElementById(fieldId);
   const inputValue = inputField.value;
@@ -73,4 +14,16 @@ function inputValue(fieldId) {
 function setElementInnerText(elemetID, area) {
   const element = document.getElementById(elemetID);
   element.innerText = area;
+}
+
+function addToAreaEntry(areaType, area) {
+  const calculationEntry = document.getElementById("calculation-entry");
+  const count = calculationEntry.childElementCount;
+  const p = document.createElement("p");
+  p.classList.add("my-4", "space-x-4");
+  p.innerHTML = ` ${
+    count + 1
+  }. ${areaType}   <span class="font-bold"> ${area}cm<sup>2</sup> </span> <button class="btn btn-success btn-sm">Convert</button>`;
+
+  calculationEntry.appendChild(p);
 }
--- script/click-function.js
+++ script/click-function.js
@@ -1,22 +1,26 @@
 document
   .getElementById("triangle-calculation")
   .addEventListener("click", function () {
-    const area = areaTriangleCalculation();
+    const base = inputValue("triangle-base");
 
-    const areaValue = document.getElementById("triangle-area-value");
+    const height = inputValue("triangle-height");
 
-    areaValue.innerText = area;
+    const area = 0.5 * base * height;
+    setElementInnerText("triangle-area-value", area);
+    addToAreaEntry("Triangle", area);
   });
 
 // rectangle
 document
   .getElementById("rectangle-calculation")
   .addEventListener("click", function () {
-    const area = areaRectangleCalculation();
+    const width = inputValue("rectangle-width");
 
-    const areaValue = document.getElementById("rectangle-area-value");
+    const length = inputValue("rectangle-length");
 
-    areaValue.innerText = area;
+    const area = width * length;
+    setElementInnerText("rectangle-area-value", area);
+    addToAreaEntry("Rectangle", area);
   });
 
 //Parallelogram
@@ -30,15 +34,47 @@ document
 
     const area = base * height;
     setElementInnerText("parallelogram-area-value", area);
+    addToAreaEntry("Parallelogram", area);
+  });
+//rhombus
+document
+  .getElementById("rhombus-calculation")
+  .addEventListener("click", function () {
+    const d1 = inputValue("rhombus-d1");
+    const d2 = inputValue("rhombus-d2");
+
+    const area = 0.5 * d1 * d2;
+
+    setElementInnerText("rhombus-area-value", area);
+
+    addToAreaEntry("Rhombush", area);
   });
 
+//pentagon
+
+document
+  .getElementById("pentagon-calculation")
+  .addEventListener("click", function () {
+    const p = inputValue("pentagon-p");
+    const a = inputValue("pentagon-a");
+
+    const area = 0.5 * p * a;
+
+    setElementInnerText("pentagon-area-value", area);
+
+    addToAreaEntry("Pentagon", area);
+  });
+
+//ellipse
 document
   .getElementById("ellipse-calculation")
   .addEventListener("click", function () {
     const major = inputValue("ellipse-major");
     const minor = inputValue("ellipse-minor");
 
-    const area = Math.PI * major * major;
+    const area = Math.PI * major * minor;
     const areaTwoDecimal = area.toFixed(2);
-    setElementInnerText("ellipse-area-value", area);
+    setElementInnerText("ellipse-area-value", areaTwoDecimal);
+
+    addToAreaEntry("Ellipse", areaTwoDecimal);
   });
